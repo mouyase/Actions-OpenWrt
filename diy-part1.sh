e9f226636ba8bd7ef6e9698848246b29d07f0e2a
某亚瑟 <mouyase@qq.com>
2022-07-28 02:40:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,4 +29,4 @@ sed -i "s/3.openwrt.pool.ntp.org/ntp3.aliyun.com/" package/base-files/files/bin/
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
