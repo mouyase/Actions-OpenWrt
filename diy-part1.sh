adafe70786ac1a4712b0bcbe9814c2b3b053dbb3
wb_guodonghao <wb_guodonghao@meituan.com>
2022-07-28 16:00:24 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,22 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 修改默认IP为192.168.5.1
-# sed -i 's/lan) ipad=${ipaddr:-"192.168.1.1"} ;;/lan) ipad=${ipaddr:-"192.168.5.1"} ;;/' package/base-files/files/bin/config_generate
-
-# 修改默认使用dnsmasq-full
-sed -i 's/dnsmasq/dnsmasq-full/' include/target.mk
-
-# 修改rom信息
-# sed -i 's/VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),OpenWrt)/VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),Yojigen OpenWrt)/' include/version.mk
-
-# 修改时间校准服务器和时区
-sed -i "s/UTC/Asia\/Shanghai/" package/base-files/files/bin/config_generate
-sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/" package/base-files/files/bin/config_generate
-sed -i "s/1.openwrt.pool.ntp.org/ntp1.aliyun.com/" package/base-files/files/bin/config_generate
-sed -i "s/2.openwrt.pool.ntp.org/ntp2.aliyun.com/" package/base-files/files/bin/config_generate
-sed -i "s/3.openwrt.pool.ntp.org/ntp3.aliyun.com/" package/base-files/files/bin/config_generate
-
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,18 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP为192.168.5.1
+sed -i 's/192.168.1.1/192.168.5.1/' package/base-files/files/bin/config_generate
+
+# 修改默认使用dnsmasq-full
+sed -i 's/dnsmasq/dnsmasq-full/' include/target.mk
+
+# 修改rom信息
+# sed -i 's/VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),OpenWrt)/VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),Yojigen OpenWrt)/' include/version.mk
+
+# 修改时间校准服务器和时区
+sed -i 's/UTC/CST-8/' package/base-files/files/bin/config_generate
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp1.aliyun.com/' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp2.aliyun.com/' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/ntp3.aliyun.com/' package/base-files/files/bin/config_generate
