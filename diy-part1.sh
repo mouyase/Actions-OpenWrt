e7fe5ba87c45cf9234f7b7e80ac5ff0265bb0b6d
某亚瑟 <mouyase@qq.com>
2022-07-28 09:56:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改默认IP为192.168.5.1
-sed -i 's/lan) ipad=${ipaddr:-"192.168.1.1"} ;;/lan) ipad=${ipaddr:-"192.168.5.1"} ;;/' package/base-files/files/bin/config_generate
+# sed -i 's/lan) ipad=${ipaddr:-"192.168.1.1"} ;;/lan) ipad=${ipaddr:-"192.168.5.1"} ;;/' package/base-files/files/bin/config_generate
 
 # 修改默认使用dnsmasq-full
 sed -i 's/dnsmasq/dnsmasq-full/' include/target.mk
@@ -20,7 +20,7 @@ sed -i 's/dnsmasq/dnsmasq-full/' include/target.mk
 # sed -i 's/VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),OpenWrt)/VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),Yojigen OpenWrt)/' include/version.mk
 
 # 修改时间校准服务器和时区
-sed -i "s/set system.@system[-1].timezone='UTC'/set system.@system[-1].timezone='Asia\/Shanghai'/" package/base-files/files/bin/config_generate
+sed -i "s/UTC/Asia\/Shanghai/" package/base-files/files/bin/config_generate
 sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/" package/base-files/files/bin/config_generate
 sed -i "s/1.openwrt.pool.ntp.org/ntp1.aliyun.com/" package/base-files/files/bin/config_generate
 sed -i "s/2.openwrt.pool.ntp.org/ntp2.aliyun.com/" package/base-files/files/bin/config_generate
